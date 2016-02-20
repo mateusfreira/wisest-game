2cf9d1d0e14fe604ddbff5c7bb801af1e984356d
Mateus Freira <mateus.freira@gmail.com>
2016-02-20 16:01:51 -0200

    Fix database

--- rest/controllers/Game.js
+++ rest/controllers/Game.js
@@ -11,7 +11,7 @@ module.exports = {
   start: function(req, res, next) {
     GameService.start(req.session, {
       player: {
-        _id : "56c892ce283c617e7c8b0ed4",
+        _id: "56c892ce283c617e7c8b0ed4",
         name: "Jon due!",
       },
       theme: req.body.theme,
