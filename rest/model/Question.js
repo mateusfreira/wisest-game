027582eabf7b7ee123f875cb733c5126a7025c72
Mateus Freira <mateus.freira@gmail.com>
2016-02-20 11:42:54 -0200

    Refactory questions and gameservice

--- rest/model/Question.js
+++ rest/model/Question.js
@@ -17,12 +17,20 @@ QuestionSchema.methods.checkAnswer = function(answer) {
 QuestionSchema.statics.some = function(context) {
 	var self = this;
 	return self
-			.count()
-			.then(function(count) {
-				var random = Math.floor(Math.random() * count);
-				return self.findOne().skip(random);
-			});
+		.count()
+		.then(function(count) {
+			var random = Math.floor(Math.random() * count);
+			return self.findOne().skip(random);
+		});
 };
+
+QuestionSchema.statics.checkAnswerById = function(id, answer) {
+	var self = this;
+	return self.findById(id)
+		.then(function(question) {
+			return question.checkAnswer(answer);
+		});
+}
 
 var Question = mongoose.model('Question', QuestionSchema);
 module.exports = Question;
--- rest/services/GameService.js
+++ rest/services/GameService.js
@@ -1,6 +1,32 @@
-const GameService = function(){
-  this.score = function(context){
-    console.log("Not implemented yet!");
-  };
+const requireModule = require('../model/index').requireModule;
+const Question = requireModule("Question");
+const GameService = function() {
+	var self = this;
+	this.score = function(context) {
+		console.log("Not implemented yet!");
+		return {
+			value: "You are the best!"
+		};
+	};
+
+	this.lose = function(context, question, answer) {
+		console.log("Not implemented yet!");
+		return {
+			value: "You can do it better!"
+		};
+	};
+
+	this.answerQuestion = function(context, question, answer) {
+		return Question.checkAnswerById(question, answer)
+			.then(function(isItRight) {
+				var result;
+				if (isItRight) {
+					result = self.score(context);
+				} else {
+					result = self.lose(context, question, answer);
+				}
+				return result;
+			});
+	};
 };
 module.exports = new GameService();
