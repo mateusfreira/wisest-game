ece637e56b1699fccd8d8eaab8d76ae35c228932
Mateus Freira <mateus.freira@gmail.com>
2016-02-20 10:15:35 -0200

    Question check endpoint

--- rest/controllers/Question.js
+++ rest/controllers/Question.js
@@ -1,11 +1,20 @@
 var requireModule = require('../model/index').requireModule;
-
+const Question = requireModule("Question");
 module.exports = {
   some : function(req, res, next) {
-  	const Question = requireModule("Question");
   	Question.find().then(function(question){
-
   		res.status(200).send(question);	
+  	});
+  },
+  check : function(req, res, next) {
+  	var question = req.query.id;
+  	var answer = req.query.answer;
+  	Question.findById(question)
+  	.then(function(question){
+  		res.status(200).send(question);	
+  	}).catch(function(e){
+  		console.log(e);
+  		res.status(500).send(e);	
   	});
   }
 };
