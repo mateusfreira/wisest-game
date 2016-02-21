d54255cf59b053530b0538726ff730cf0e708242
Mateus Freira <mateus.freira@gmail.com>
2016-02-21 08:28:10 -0300

    User on single-player window

---
+++ rest/controllers/User.js
@@ -0,0 +1,9 @@
+const requireModule = require('../model/index').requireModule,
+      User = requireModule("User"),
+      reponseWithPromise = require('./Utility').reponseWithPromise;
+      
+module.exports = {
+  current: function(req, res) {
+    res.status(200).send({ first_name : req.user.first_name, id : req.user._id });
+  }
+};
