9c90c8502ab4217e869dfba37036fcace4d7bfe8
Jhoterdan de Paula <jotaskt@gmail.com>
2016-02-21 07:20:16 -0300

    resolve conflicts index.js

--- rest/model/index.js
+++ rest/model/index.js
@@ -1,11 +1,6 @@
-<<<<<<< Updated upstream
-var mongoose = require('mongoose'),
-    dbUri = process.env.MONGO_URI || 'mongodb://192.168.1.10/wisest-game';
-    
-=======
 var mongoose = require('mongoose');
 var dbUri = process.env.MONGO_URI || 'mongodb://localhost/wisest-game';
->>>>>>> Stashed changes
+
 mongoose.connect(dbUri);
 
 module.exports = {
