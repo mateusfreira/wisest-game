56e3242ed907b5d8fef9525af62de9fb61e0e9cd
Airton Cabral <airtoncbr@gmail.com>
2016-02-21 22:18:08 -0300

    Changed navigation on game start.

--- client/modules/game/js/controller/StartGameController.js
+++ client/modules/game/js/controller/StartGameController.js
@@ -1,22 +1,45 @@
-angular.module("WisestGame").controller('StartGameController', ['$state', 'Themes', 'Modes', 'Game', function($state, Themes, Modes, Game) {
+angular.module("WisestGame")
+	.controller('StartGameController', ['$scope', '$state', 'Themes', 'Modes', 'Game',
+		function($scope, $state, Themes, Modes, Game) {
 
-	var self = this;
+			var self = this;
 
-	this.modes = Modes.query();
-	this.themes = Themes.query();
-	this.gameContext = {};
+			this.modes = Modes.query();
+			this.themes = Themes.query();
+			this.gameContext = {};
 
-	this.startGame = function() {
-		Game.startGame.query({
-			mode: this.gameContext.mode._id,
-			theme: this.gameContext.theme._id
-		}).$promise
-		.then(function(response) {
-			$state.go("playGame", {}, {location: true});
-		})
-		.catch(function(err){
-			console.log(err);
-		});
-	};
+			this.chooseMode = function(mode) {
+				$scope.selectedMode = mode;
+			};
 
-}]);
+			this.chooseTheme = function(theme) {
+				$scope.selectedTheme = theme;
+			};
+
+			this.backToMode = function() {
+				$scope.selectedMode = undefined;
+			};
+
+			this.backToTheme = function() {
+				$scope.selectedTheme = undefined;
+			};
+
+			this.startGame = function() {
+
+				this.gameContext.mode = $scope.selectedMode;
+				this.gameContext.theme = $scope.selectedTheme;
+
+				Game.startGame.query({
+					mode: $scope.selectedMode._id,
+					theme: $scope.selectedTheme._id
+				}).$promise
+				.then(function(response) {
+					$state.go("playGame", {}, {location: true});
+				})
+				.catch(function(err){
+					console.log(err);
+				});
+			};
+		}
+	]
+);
