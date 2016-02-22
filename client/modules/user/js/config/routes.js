b04d2bde5d9da464e6a6ed90bfae5e6100814a75
Mateus Freira <mateus.freira@gmail.com>
2016-02-22 00:12:59 -0300

    Refactory duplicated request

--- client/modules/user/js/config/routes.js
+++ client/modules/user/js/config/routes.js
@@ -7,30 +7,26 @@ angular.module("WisestGame").config(['$stateProvider', '$urlRouterProvider', fun
         .state('profile', {
             url: "/user/profile",
             templateUrl: "modules/user/template/profile.html",
-            data: {pageTitle: 'Profile'},
-            controller: "ProfileController"
+            data: {pageTitle: 'Profile'}
         })
 
         // User
         .state('signup', {
             url: "/user/signup",
             templateUrl: "modules/user/template/signup.html",
-            data: {pageTitle: 'Sign Up'},
-            controller: "SignupController"
+            data: {pageTitle: 'Sign Up'}
         })
 
         .state('login', {
             url: "/user/login",
             templateUrl: "modules/user/template/login.html",
-            data: {pageTitle: 'Login'},
-            controller: "LoginController"
+            data: {pageTitle: 'Login'}
         })
 
         .state('forgot', {
             url: "/user/forgot",
             templateUrl: "modules/user/template/forgot.html",
-            data: {pageTitle: 'Recover password'},
-            controller: "LoginController"
+            data: {pageTitle: 'Recover password'}
         });
 
 }]);
