e829926fcf0c745f617ec7668d2127db0ce4a52a
Airton Cabral <airtoncbr@gmail.com>
2016-02-22 02:48:10 -0300

    Completed sounds and navigation. Still buggy.

--- client/modules/user/js/directives/gameTab.js
+++ client/modules/user/js/directives/gameTab.js
@@ -1,12 +1,21 @@
 Mousetrap.bind("enter", function() {
-	$("#enter-sound")[0].play();
+	if(!$(currentItem).parents(".playing-game").toArray().length) {
+		$("#enter-sound")[0].play();
+	}
 });
 
 Mousetrap.bind("up", function() {
 	var currentItem = $(window.currentItem)[0];
 	var items = $("[game-tab]").toArray();
 	var currentIndex = items.indexOf(currentItem);
-	var nextIndex = currentIndex - 1 % items.length;
+	var nextIndex;
+
+	if($(currentItem).parents(".playing-game").toArray().length) {
+		nextIndex = currentIndex - 2 % items.length;
+	} else {
+		nextIndex = currentIndex - 1 % items.length;
+	}
+
 
 	for(var i=0; i<items.length; i++) {
 		if(i == nextIndex) {
@@ -21,7 +30,55 @@ Mousetrap.bind("down", function() {
 	var currentItem = $(window.currentItem)[0];
 	var items = $("[game-tab]").toArray();
 	var currentIndex = items.indexOf(currentItem);
-	var nextIndex = currentIndex + 1 % items.length;
+	var nextIndex;
+
+	if($(currentItem).parents(".playing-game").toArray().length) {
+		nextIndex = currentIndex + 2 % items.length;
+	} else {
+		nextIndex = currentIndex + 1 % items.length;
+	}
+
+	for(var i=0; i<items.length; i++) {
+		if(i == nextIndex) {
+			window.currentItem = items[i];
+			$(window.currentItem).focus();
+			$("#menu-sound")[0].play();
+		}
+	}
+});
+
+Mousetrap.bind("left", function() {
+	var currentItem = $(window.currentItem)[0];
+	var items = $("[game-tab]").toArray();
+	var currentIndex = items.indexOf(currentItem);
+	var nextIndex;
+
+	if($(currentItem).parents(".playing-game").toArray().length) {
+		nextIndex = currentIndex - 1 % items.length;
+	} else {
+		nextIndex = currentIndex - 0 % items.length;
+	}
+
+	for(var i=0; i<items.length; i++) {
+		if(i == nextIndex) {
+			window.currentItem = items[i];
+			$(window.currentItem).focus();
+			$("#menu-sound")[0].play();
+		}
+	}
+});
+
+Mousetrap.bind("right", function() {
+	var currentItem = $(window.currentItem)[0];
+	var items = $("[game-tab]").toArray();
+	var currentIndex = items.indexOf(currentItem);
+	var nextIndex;
+
+	if($(currentItem).parents(".playing-game").toArray().length) {
+		nextIndex = currentIndex + 1 % items.length;
+	} else {
+		nextIndex = currentIndex + 0 % items.length;
+	}
 
 	for(var i=0; i<items.length; i++) {
 		if(i == nextIndex) {
@@ -39,7 +96,7 @@ angular.module("WisestGame").directive('gameTabHelper', function($timeout) {
 			setTimeout(function() {
 				$element.find("[game-tab]:first").focus();
 				window.currentItem = $element.find("[game-tab]:first")[0];
-			}, 500);
+			}, 700);
 		}
 	};
 });
